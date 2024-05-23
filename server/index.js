8e3b52de65b3a2aed3fcf2d34312364eb5e39f24
Gauravtheone <121490722+Gauravtheone@users.noreply.github.com>
2024-05-23 23:55:11 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser())
 app.use(
     cors({
-        origin:"http://localhost:3000",credentials: true
+        origin:"https://study-notionapp.vercel.app",credentials: true
     })
 )
 
