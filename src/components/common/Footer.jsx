a4457e9ef51ba8eedd9db24757648ad804d712a9
Gauravtheone <121490722+Gauravtheone@users.noreply.github.com>
2024-06-26 17:04:46 +0530

    Update Footer.jsx

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -172,7 +172,7 @@ const Footer = () => {
             })}
           </div>
 
-          <div className="text-center">Made with ❤️ CodeHelp © 2023 Studynotion</div>
+          <div className="text-center">Made with ❤️ by Gaurav saini </div>
         </div>
       </div>
     </div>
